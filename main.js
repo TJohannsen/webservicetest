26f3b77fd0ac3189163fd25340406b0518524170
tjohannsen <thorejohannsen@gmx.net>
2016-10-11 22:35:06 +0200

    changed send to end

--- main.js
+++ main.js
@@ -9,19 +9,13 @@ app.use(bodyParser.json());
 app.post("/newtext", function (req, res) {
     var text = req.body.text;
     console.log(text);
-    res.send("Successful!");
+    res.end("Successful!");
 });
 
 app.get("/hello", function(req, res) {
     console.log("hi there!");
-    res.send("Hey back!");
+    res.end("Hey back!");
 });
-
-/*var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Example app listening at http://%s:%s", host, port)
-}); */
 
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
